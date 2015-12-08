377e218ba6f28fe5d01f1a1bf9048fbb5fa8e137
Aleš Roubíček <ales@roubicek.name>
2015-12-08 11:16:30 +0100

    Improved settings and documentation

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # white-lady
+
 Mocha interface for Nightmare driven tests
 
 ```
-npm install --save-dev white-lady mocha-generators
+npm install --save-dev white-lady mocha mocha-generators chai
 mocha --ui white-lady
 ```
+
+Simple test scenario:
 
 ```
 require('mocha-generators').install();
@@ -17,3 +20,22 @@ describePage('Home page', '/', function() {
   });
 });
 ```
+
+## Environment variables
+
+You can change default behavior with ENV variables.
+
+* `PORT` - you can change localhost port, default is `3001`
+* `ROOT_URL` - you can change server URL, default is `http://localhost:${PORT}`
+* `SHOW_WINDOW` - handy for debugging, shows browser window and does not close it
+
+## UI automation API
+
+We are using [Nightmare](http://nightmarejs.org) for UI automation.
+[See the API](https://github.com/segmentio/nightmare#api)
+
+White lady extends Nightmare prototype with handy helper functions
+
+### .textOf(selector)
+
+Reads the text content of selected element.
--- index.js
+++ index.js
@@ -1,31 +1,57 @@
 /**
  * @copyright 2015 Topmonks s.r.o.
  */
+
+const debug = require('debug')('nightmare');
 const Mocha = require('mocha');
 const Suite = require('mocha/lib/suite');
 const Test = require('mocha/lib/test');
 const Nightmare = require('nightmare');
 const url = require('url');
 
-const DEBUG = Boolean(process.env.DEBUG);
-const ROOT_URL = process.env.ROOT_URL || 'http://localhost:3001';
+const PORT = process.env.PORT || 3001;
+const ROOT_URL = process.env.ROOT_URL || `http://localhost:${PORT}`;
+const SHOW_WINDOW = process.env.SHOW_WINDOW;
 console.log('Testing server:', ROOT_URL);
 const page = path => url.resolve(ROOT_URL, path);
 
-const text = function(selector, done) {
+const textOf = function(selector, done) {
+    debug('.textOf() %s', selector);
     this._evaluate(function(selector) {
         var el = document.querySelector(selector);
         return el.textContent;
     }, done, selector);
 };
 
+/**
+ * Reads text content of element
+ * @param selector Element selector
+ * @param done Callback
+ * @returns {Nightmare}
+ */
 Nightmare.prototype.textOf = function() {
+    debug('queueing action "textOf"');
     var args = [].slice.call(arguments);
-    this._queue.push([text, args]);
+    this._queue.push([textOf, args]);
     return this;
 };
 
 /**
+ * Mocha interface for Nightmare driven UI tests.
+ *
+ * ```
+ * require('mocha-generators').install();
+ * const expect = require('chai').expect;
+ *
+ * describePage('Home page', '/', function() {
+ *   it('should have page title', function*() {
+ *     const title = yield this.page.title();
+ *     expect(title).to.equal('page title');
+ *   });
+ * });
+ * ```
+ *
+ * Actual page is always in scope context variable `this.page`
  *
  * @param {Suite} suite Root suite.
  * @type {Function}
@@ -56,9 +82,9 @@ module.exports = Mocha.interfaces['white-lady'] = function(suite) {
 
         /**
          * Describe a page "suite"
-         * @param title
-         * @param path page path
-         * @param fn
+         * @param title Test title
+         * @param path Tested Page path
+         * @param fn Test implementation
          * @returns {Suite}
          */
         context.describePage = function(title, path, fn) {
@@ -68,13 +94,14 @@ module.exports = Mocha.interfaces['white-lady'] = function(suite) {
 
             var nightmare;
             suite.beforeAll(function() {
-                this.page = nightmare = new Nightmare({ show: DEBUG });
+                this.page = nightmare = new Nightmare({ show: SHOW_WINDOW });
             });
             suite.beforeEach(function() {
                 nightmare.goto(page(path));
             });
             suite.afterAll(function(done) {
                 this.page = null;
+                if (SHOW_WINDOW) return;
                 nightmare.end(done);
             });
 
@@ -133,4 +160,3 @@ module.exports = Mocha.interfaces['white-lady'] = function(suite) {
         };
     });
 };
-
